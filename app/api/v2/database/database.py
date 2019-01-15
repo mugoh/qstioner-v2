50acd1ea438bb1edbce2c53555a0b1eb8048fe01
root <mugoh.ks@gmail.com>
2019-01-15 15:43:39 +0300

    [Chore #163220908] Define function to query the db

--- app/api/v2/database/database.py
+++ app/api/v2/database/database.py
@@ -2,13 +2,15 @@ import psycopg2
 
 from .config import config_db
 
+connection = None
+
 
 class Database:
 
     def init_db(self):
-        conn = psycopg2.connect(**config_db())
+        connection = psycopg2.connect(**config_db())
 
-        return conn
+        return connection
 
     @classmethod
     def create_tables(cls):
@@ -19,5 +21,30 @@ class Database:
             CREATE_TABLE_QUESTIONS,
             CREATE_TABLE_TOKENS]
 
+        print("Created Tables")
+
         for statement in statements:
             query_db(statement)
+
+
+def query_db(statement, rowcount=False, return_value=False, one=False, many=False):
+
+    try:
+        global connection
+
+        connection = db_instance.init_db()
+        cursor = connection.cursor()
+
+        connection.commit()
+        cursor.close()
+
+    except (Exception, psycopyg2.DatabaseError) as er:
+        print(er)
+    finally:
+        if connection:
+            connection.close()
+
+    return
+
+
+db_instance = Database()
