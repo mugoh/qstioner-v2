157f4bfafe7c7aa4cfca45503fa0bd5c3a1da5f4
root <mugoh.ks@gmail.com>
2019-01-15 14:27:06 +0300

    [Chore #163220908] Create database tables

--- app/api/v2/database/database.py
+++ app/api/v2/database/database.py
@@ -1,1 +1,23 @@
 import psycopg2
+
+from .config import config_db
+
+
+class Database:
+
+    def init_db(self):
+        conn = psycopg2.connect(**config_db())
+
+        return conn
+
+    @classmethod
+    def create_tables(cls):
+        statements = [
+            CREATE_TABLE_USERS,
+            CREATE_TABLE_MEETUPS,
+            CREATE_TABLE_RSVPS,
+            CREATE_TABLE_QUESTIONS,
+            CREATE_TABLE_TOKENS]
+
+        for statement in statements:
+            query_db(statement)
