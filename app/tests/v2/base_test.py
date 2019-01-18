e3a3c68af6f12347d71bc0a78e76d75dcfa96261
root <mugoh.ks@gmail.com>
2019-01-18 09:10:16 +0300

    [Chore #163309944] Initialize tables in class context attribute

--- app/tests/v2/base_test.py
+++ app/tests/v2/base_test.py
@@ -14,7 +14,8 @@ class BaseTestCase(unittest.TestCase):
         self.client = self.app.test_client()
 
         with self.app.app_context():
-            db_instance.init_db()
+            db_instance.drop_tables()
+            db_instance.create_tables()
 
         # create new user
         self.user_data = json.dumps(dict(
@@ -27,13 +28,18 @@ class BaseTestCase(unittest.TestCase):
             password="pa55word"))
 
         response = self.client.post('/api/v1/auth/register',
-                                    data=self.user_data,
+                                    data=json.dumps(dict(
+                                        username="DomesticableCow",
+                                        email="cow@mammals.milkable",
+                                        password="pa55word"
+                                    )),
                                     content_type='application/json')
         self.new_user = json.loads(response.data.decode())
 
         login_response = self.client.post('/api/v1/auth/login',
                                           data=self.user_data,
                                           content_type='application/json')
+        print(login_response.get_json())
         user = json.loads(login_response.data.decode()
                           ).get("Data")[0].get('token')
         self.auth_header = {"Authorization": "Bearer " + user.split("'")[1]}
@@ -62,6 +68,7 @@ class BaseTestCase(unittest.TestCase):
                                     )),
                                     content_type='application/json')
         # Get Authorization token
+        print(user_res.get_json())
 
         userH = user_res.get_json().get('Data')[0].get('token')
         self.admin_auth = {"Authorization": "Bearer " + userH}
@@ -93,3 +100,6 @@ class BaseTestCase(unittest.TestCase):
                               content_type='application/json',
                               headers=headers)
         return res
+
+    def teardown(self):
+        db_instance.drop_tables()
