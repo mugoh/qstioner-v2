6cdfc1fd7ed4ecc8a5edcba1be76677460e86351
root <mugoh.ks@gmail.com>
2019-01-19 17:43:58 +0300

    [Feature #163342614] Allow special characters in comments

--- app/api/v2/views/comments.py
+++ app/api/v2/views/comments.py
@@ -25,7 +25,7 @@ class Comments(Resource):
         parser = reqparse.RequestParser(trim=True, bundle_errors=True)
 
         parser.add_argument('body', required=True,
-                            type=inputs.regex('^[A-Za-z0-9_ ]+$'),
+                            type=inputs.regex('^[A-Za-z0-9_ ?/!.,"\\\':;]+$'),
                             help="Is that readable? Provide a valid comment")
 
         args = parser.parse_args(strict=True)
