b2c602557853d6ec79b19e47e2cfd2538079a794
root <mugoh.ks@gmail.com>
2019-01-19 22:21:02 +0300

    [Chore #163344595] Test creation of new comment

--- app/tests/v2/comment_test.py
+++ app/tests/v2/comment_test.py
@@ -1,3 +1,5 @@
+import json
+
 from .base_test import BaseTestCase
 
 
@@ -6,8 +8,29 @@ class CommentTest(BaseTestCase):
     def test_get_comments_for_empty_table(self):
         res = self.get('api/v1/questions/1/comment')
 
-        print(res.get_json())
-
         self.assertEqual(res.get_json().get('Data'),
                          [],
                          msg="Fails. Initiliazes comments table with data")
+
+    def test_get_comments_for_missing_question(self):
+        res = self.get('api/v1/questions/404/comment')
+
+        expected_response = 'Question of ID 404 non-existent'
+        self.assertTrue(
+            expected_response
+            in res.get_json().get('Message'),
+            msg="Fails. Allows user to get comments of non-existent question")
+
+    def test_create_comment_for_question(self):
+        data = json.dumps(dict(
+            body="I'll be there for the cookies"
+        ))
+
+        res = self.post('api/v1/questions/1/comment',
+                        data=data)
+
+        self.assertEqual(res.get_json().get('Data')[0].get('body'),
+                         "I'll be there for the cookies",
+                         msg="Fails to create  comment")
+
+    def test_create_comment_for_missing_question(self):
