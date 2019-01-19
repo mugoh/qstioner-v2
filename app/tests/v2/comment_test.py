e314389f65040806061493eb07b4346bb56ae23a
root <mugoh.ks@gmail.com>
2019-01-19 23:16:05 +0300

    [Chore #163344595] Test storage of posted comments

--- app/tests/v2/comment_test.py
+++ app/tests/v2/comment_test.py
@@ -38,3 +38,24 @@ class CommentTest(BaseTestCase):
                         data=self.qcomment)
         self.assertTrue('non-existent' in res.get_json().get('Message'),
                         msg="Fails. Posts comment to missing question")
+
+    def test_posted_comment_exists_in_record(self):
+        self.post('api/v1/questions/1/comment',
+                  data=self.qcomment)
+
+        res = self.get('api/v1/questions/1/comment')
+
+        self.assertNotEqual(res.get_json().get('Data'),
+                            [],
+                            msg="Fails to store posted comment\
+                            in comments table")
+
+    def test_post_same_comment_twice(self):
+        self.post('api/v1/questions/1/comment',
+                  data=self.qcomment)
+        res = self.post('api/v1/questions/1/comment',
+                        data=self.qcomment)
+
+        self.assertEqual('Looks like You have posted this comment before',
+                         res.get_json().get('Message'),
+                         "Fails. Allows a user to re-post the same comment")
