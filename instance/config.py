f55fa8e904a62a35e636b60256a0af6007ee8e09
root <mugoh.ks@gmail.com>
2019-01-19 11:27:18 +0300

    [Starts #163342614] Uncomment secret key config

--- instance/config.py
+++ instance/config.py
@@ -4,9 +4,10 @@ import os
 class BaseConfig:
     DEBUG = False
     TESTING = False
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    #SECRET_KEY = os.environ.get('SECRET_KEY')
     CSRF_ENABLED = True
     BUNDLE_ERRORS = True
+    SECRET_KEY = b'\xc2;F]l\x0490u&6t\xfe\x08'
     AUTH_TOKEN_EXP_DAYS = 30
     AUTH_TOKEN_EXP_SECS = 60 * 5
 
