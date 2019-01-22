3953c072d01390771550f2f984eb2a1acab50764
root <mugoh.ks@gmail.com>
2019-01-22 19:30:34 +0300

    [Chore #163391872] Teste sending of put requests to comments

--- app/tests/v2/comment_test.py
+++ app/tests/v2/comment_test.py
@@ -5,7 +5,7 @@ from .base_test import BaseTestCase
 
 class CommentTest(BaseTestCase):
 
-    def test_get_comments_for_empty_table(self):
+    """def test_get_comments_for_empty_table(self):
         res = self.get('api/v1/questions/1/comment')
 
         self.assertEqual(res.get_json().get('Data'),
@@ -92,4 +92,48 @@ class CommentTest(BaseTestCase):
         print(res.get_json())
         self.assertEqual(res.get_json().get('Data'),
                          [],
-                         msg="Fails to retrieve comments for user")
+                         msg="Fails to retrieve comments for user")"""
+
+    def test_edit_missing_user_comments(self):
+        data = json.dumps(dict(
+            body="I'll be there for the cookies"
+        ))
+        res = self.client.put('api/v1/comments/1',
+                              data=data,
+                              headers=self.auth_header,
+                              content_type='application/json')
+        self.assertTrue('not posted any' in
+                        res.get_json().get('Message'),
+                        "Fails. Allows user to edit they did not post")
+
+    def test_edit_comment(self):
+        data = json.dumps(dict(
+            body="I'll be there for the cookies"
+        ))
+        res = self.post('api/v1/questions/1/comment',
+                        data=data)
+
+        res = self.client.put('api/v1/comments/1',
+                              data=data,
+                              headers=self.auth_header,
+                              content_type='application/json')
+
+        self.assertEqual('Comment Updated',
+                         res.get_json().get('Message'),
+                         "Fails to edit user quesiton")
+
+    def test_edit_comment_with_missing_id(self):
+        data = json.dumps(dict(
+            body="I'll be there for the cookies"
+        ))
+        res = self.post('api/v1/questions/1/comment',
+                        data=data)
+
+        res = self.client.put('api/v1/comments/404',
+                              data=data,
+                              headers=self.auth_header,
+                              content_type='application/json')
+
+        self.assertEqual('Comment of ID 404 missing',
+                         res.get_json().get('Message'),
+                         "Fails to edit user quesiton")
