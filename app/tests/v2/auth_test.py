c096aea2a128cc51342e084399b4550e033bb973
root <mugoh.ks@gmail.com>
2019-01-24 15:14:52 +0300

    [Chore #163448826] Document authorization tests

--- app/tests/v2/auth_test.py
+++ app/tests/v2/auth_test.py
@@ -10,6 +10,10 @@ class AuthTestCases(BaseTestCase):
     """
 
     def test_get_registered_users(self):
+        """
+            Verify endpoint for fetching all current users
+            in the application.
+        """
         response = self.client.get('/api/v1/auth/register',
                                    data=self.user_data,
                                    content_type='application/json')
@@ -21,6 +25,10 @@ class AuthTestCases(BaseTestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_register_already_registered_user(self):
+        """
+            Verify a user's details cannot be reused in
+            making another account.
+        """
 
         response = self.client.post('/api/v1/auth/register',
                                     data=self.user_data,
@@ -33,6 +41,10 @@ class AuthTestCases(BaseTestCase):
                          msg="Fails to registration of existing account")
 
     def test_register_with_invalid_email(self):
+        """
+            Verify a user cannot register with an email pattern
+            that's not accepted.
+        """
         user_data = json.dumps(dict(
             username="DomesticableCow",
             email="cow@mammals",
@@ -49,6 +61,10 @@ class AuthTestCases(BaseTestCase):
                         msg="Fails. Registers user with invalid email")
 
     def test_register_with_existing_username(self):
+        """
+            Tests that accounts are created with unique
+            usernames.
+        """
         user_data = json.dumps(dict(
             username="DomesticableCow",
             email="cow@mammals.milk",
@@ -66,6 +82,10 @@ class AuthTestCases(BaseTestCase):
                          msg="Fails. Registers user with existing username")
 
     def test_register_with_invalid_password(self):
+        """
+            Verify password length is met before an account
+            is created for a user with this password.
+        """
         user_data = json.dumps(dict(
             username="DomesticableCow",
             email="cow@mammals.milkable",
@@ -80,6 +100,10 @@ class AuthTestCases(BaseTestCase):
                         of length less than six characters")
 
     def test_register_with_invalid_usrname(self):
+        """
+            Tests that the username pattern is met before attempting
+            to register user with given username.
+        """
         user_data = json.dumps(dict(
             username="Domesticable Cow",
             email="cow@mammals.milkable",
@@ -94,6 +118,10 @@ class AuthTestCases(BaseTestCase):
                         to register with invalid user name")
 
     def test_register_with_digits_usrname(self):
+        """
+            Verifys rasising pf a ValueError when a registration
+            with digits username is attempted.
+        """
         user_data = json.dumps(dict(
             username="Domesticable45Cow",
             email="cow@mammals.milkable",
@@ -108,6 +136,10 @@ class AuthTestCases(BaseTestCase):
                         to register with invalid user name")
 
     def test_send_request_with_invalid_json(self):
+        """
+            Tests an error response is raised when a post
+            request is send without a valid content-type JSON header.
+        """
         response = self.client.post('/api/v1/auth/register',
                                     data=self.user_data,
                                     )
@@ -119,6 +151,10 @@ class AuthTestCases(BaseTestCase):
         self.assertEqual(response.status_code, 400)
 
     def test_login_registered_user(self):
+        """
+            Tests a registered user can login and and have access to
+            the aaplication features.
+        """
         response = self.client.post('/api/v1/auth/login',
                                     data=json.dumps(dict(
                                         username="DomesticableCow",
@@ -130,6 +166,11 @@ class AuthTestCases(BaseTestCase):
                          msg="Fails to login registered user")
 
     def test_login_with_invalid_email(self):
+        """
+            Verifies that a email pattern that doesn't match
+            a correct email format cannot be used in a login
+            request.
+        """
         user_data = json.dumps(dict(
             username="DomesticableCow",
             email="cow@mammals",
@@ -143,6 +184,10 @@ class AuthTestCases(BaseTestCase):
                         msg="Fails. Logs in user with invalid email")
 
     def test_login_with_incorrect_password(self):
+        """
+            Verifies that a user given an access session
+            logged in with a stored username and password.
+        """
         user_data = json.dumps(dict(
             username="DomesticableCow",
             email="cow@mammals.milkable",
@@ -156,6 +201,10 @@ class AuthTestCases(BaseTestCase):
                         msg="Fails. Logs in user with invalid password")
 
     def test_login_with_unregistered_email(self):
+        """
+            Tests an email not registered cannot be used in accessing
+            an account on the application.
+        """
         user_data = json.dumps(dict(
             username="DomesticableCow",
             email="cow@mammals.alien",
@@ -168,6 +217,11 @@ class AuthTestCases(BaseTestCase):
                          msg="Fails. Logs in user with unknown email")
 
     def test_logged_in_user_can_log_out(self):
+        """
+            Tests that a user with account access can
+            log out of the system and render the account inaccessible
+            until the next log in.
+        """
 
         self.client.post('/api/v1/auth/login',
                          data=self.user_data,
