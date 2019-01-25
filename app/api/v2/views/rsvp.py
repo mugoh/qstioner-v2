b355a53b3b00f6844bf14e727a5f0e79562dfb92
root <mugoh.ks@gmail.com>
2019-01-25 09:08:32 +0300

    [Chore #163448826] Send rsvp JSON response in lowercase

--- app/api/v2/views/rsvp.py
+++ app/api/v2/views/rsvp.py
@@ -42,16 +42,16 @@ class Rsvps(Resource):
 
         if response not in expected_responses:
             return {
-                "Status": 400,
-                "Message": err_msg
+                "status": 400,
+                "message": err_msg
             }, 400
 
         # Confirm existence of the meetup  to rsvp
 
         if not MeetUpModel.get_by_id(id):
             return {
-                "Status": 404,
-                "Message": "That meetup does not exist"
+                "status": 404,
+                "message": "That meetup does not exist"
             }, 404
 
         # Retrieve this user details in key, value pairs
@@ -73,13 +73,13 @@ class Rsvps(Resource):
 
         else:
             return {
-                "Status": 409,
-                "Message": "You've done that same rsvp already"
+                "status": 409,
+                "message": "You've done that same rsvp already"
             }, 409
 
         return {
-            "Status": 201,
-            "Data": [RsvpModel.zipToDict(keys, data, single=True)]
+            "status": 201,
+            "data": [RsvpModel.zipToDict(keys, data, single=True)]
         }, 201
 
 
@@ -116,8 +116,8 @@ class Rsvp(Resource):
         meetups_data = list(
             map(lambda x: MeetUpModel.get_by_id(x), meetup_ids))
 
-        return {"Status": 200,
-                "Data": [(response, meetup) for response, meetup
+        return {"status": 200,
+                "data": [(response, meetup) for response, meetup
                          in zip_longest(
                              responses, meetups_data)]}, 200
 
